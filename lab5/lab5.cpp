0cb2f95cd0b886c180a53bbdbac68a5a8838e6e0
Dmytro Hushchin <dhuchchin@gmail.com>
2021-05-01 20:19:17 +0300

    Added class ASTNode

---
+++ lab5/lab5.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+enum class Type {
+    variable,
+    constant,
+    math_operator,
+    UNDEFINED
+};
+
+
+class ASTNode {
+private:
+    string data;
+    Type type;
+    ASTNode* left;
+    ASTNode* right;
+public:
+    ASTNode();
+    ~ASTNode();
+};
+
+
+
+int main() {
+    ASTNode root;
+    system("pause");
+    return 0;
+}
+
+
+
+ASTNode::ASTNode()
+{
+    this->type = Type::UNDEFINED;
+    this->data = "";
+    this->left = nullptr;
+    this->right = nullptr;
+}
+
+ASTNode::~ASTNode()
+{
+    delete left;
+    delete right;
+}
